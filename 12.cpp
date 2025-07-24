402b50ee141460ab602eeed2f51d714f74a3a83d
9RU6EN <cliveruben123@gmail.com>
2025-07-24 13:54:18 +0530

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 5;       
+    double area = a*a;  
+    
+    cout << "Area: " << area;
+    return 0;
+}
---
+++ 13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double length, width;
+    
+    cout << "Enter length of rectangle: ";
+    cin >> length;
+    
+    cout << "Enter width of rectangle: ";
+    cin >> width;
+    
+    cout << "Area of rectangle = " << length * width;
+    
+    return 0;
+}
